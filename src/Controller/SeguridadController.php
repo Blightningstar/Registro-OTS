99cef8de7fb0fa5d88f1717b3057a888cf48c384
Daniel <110100010111h@gmail.com>
2019-04-28 05:46:23 -0600

    Docs(Seguridad): agrega documentación al controlador seguridad
    Agrega pequeñas descripciones al código

--- src/Controller/SeguridadController.php
+++ src/Controller/SeguridadController.php
@@ -13,46 +13,50 @@ use Cake\Event\Event;
  */
 class SeguridadController extends AppController
 {
-
-
-    public function beforeFilter(Event $event)
-    {
-        parent::beforeFilter($event);
-        $this->set('active_title', 'LogIn');
-    }
-
     /**
      * login
      * @author Daniel Marín <110100010111h@gmail.com>
      * 
-     * Start a new session for the authentified user.
+     * Verifies if there is any logged user:
+     *  - If there is then starts a redirection and flash a message.
+     *  - Else it waits for the user credentials to be post and checks if 
+     *    the user exists, then starts a new session for the authentified user.
+     * 
      * @return \Cake\Http\Response|null Redirects if the user credentials are correct, renders view otherwise.
      */
-    public function login()
-    {        
+    public function login(){
+        $this->set('active_title', 'TitlebarSignIn');
         $actualUser = $this->viewVars['actualUser'];
         if(!$actualUser){
             if($this->request->is('post')){
-                $user_c = new SegUsuarioController;
                 $credentials = $this->request->getData();
                 $userdata = $credentials['email'];
                 $hash = $this->hash($credentials['password']);
-                $user = $user_c->getUser($userdata,$hash);
-                if($user){
-                    $actualUser = $user;
+
+                $userController = new SegUsuarioController;
+                $actualUser = $userController->getUser($userdata,$hash);
+                
+                if(!$actualUser)
+                    $this->Flash->error(__('The username or the password are incorrect, please try again.'));
+                else{
                     $this->request->getSession()->write('actualUser',$actualUser);
                     $this->set(compact('actualUser'));
                     $this->Flash->success('Logged in succefully.');
+
+                    /**
+                     * return $this->redirect($this->referer()); 
+                     * Se encicla redireccionandose asi mismo y luego a la página requerida
+                     * No fue posible manejar esta redireccioón eniclada se optó por
+                     * redireccionar a la página principal luego del logueo.
+                     */
+
                     return $this->redirect(['controller'=>'MainPage','action' => 'index']);
-                    
-                }else{
-                    $this->Flash->error('The username or the password are incorrect, please try again.');
-                }                
-            }
+                }
+           }
         }else{
             $this->Flash->error('You are already logged in.');
-            return $this->redirect(['controller'=>'MainPage','action' => 'index']);
-
+            return $this->redirect($this->referer());
+            //return $this->redirect(['controller'=>'MainPage','action' => 'index']);
         }
     }
 
@@ -60,7 +64,9 @@ class SeguridadController extends AppController
      * logout
      * @author Daniel Marín <110100010111h@gmail.com>
      *
-     * Finishes actual session.
+     * Deletes the actual user data and finishes the actual session, it doesn't
+     * deletes nothing if the user has not logged in.
+     * 
      * @return \Cake\Http\Response.
      */
     public function logout()
@@ -86,7 +92,9 @@ class SeguridadController extends AppController
      * @author Daniel Marín <110100010111h@gmail.com>
      *
      * Checks if the given user exists, sends an email with a restauration code and 
-     * saves the restauration code on the database for the given user.
+     * saves the restauration code on the database for the given user, it doesn't 
+     * sends nothing if the user doesn\'t exist or if the user is already logged in.
+     * 
      * @return \Cake\Http\Response|null Redirects if the user exists, renders view otherwise.
      */
     public function restoreSend()
@@ -112,7 +120,7 @@ class SeguridadController extends AppController
                     }else if($user_code){
 
                         // TODO: Change error message to a warning message, fix warning bug.
-                        // $this->Flash->warning('Code already sent, please check the email.');
+                        // $this->Flash->warning('Code already sent  to ' . $email . ', please check your email.');
 
                         $this->Flash->error('Code already sent  to ' . $email . ', please check your email.');                    
                        
@@ -134,7 +142,8 @@ class SeguridadController extends AppController
      * restoreVerify
      * @author Daniel Marín <110100010111h@gmail.com>
      * 
-     * change tha actual password for a new one with a restauration code.
+     * change tha actual password for a new one requiring a restauration code.
+     * 
      * @return \Cake\Http\Response|null Redirects on successful change, renders view otherwise.
      */
     public function restoreVerify($email)
@@ -172,6 +181,7 @@ class SeguridadController extends AppController
      * @author Daniel Marín <110100010111h@gmail.com>
      *
      * Changes the actual user password for a new one.
+     * 
      * @return \Cake\Http\Response|null Redirects on successful change or if user not logged, renders view otherwise.
      */
     public function change()
